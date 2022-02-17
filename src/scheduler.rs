626b8cc6b3ccac28ecb98783f70b81fdf9e1ed6a
Frank Fenton <echelon.iiii@gmail.com>
2022-02-17 18:25:41 +1100

    feat: make mode change happen at boot and wake

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Context, Result};
 use std::env::current_exe;
 use std::process::Command;
+use tempfile::NamedTempFile;
 
 const TASK_NAME: &str = "razer_keyboard_mode_changer";
 
@@ -24,32 +25,32 @@ pub fn uninstall_from_schedule() -> Result<()> {
 
 pub fn schedule_self_if_not_scheduled() -> Result<()> {
     let exe_path = current_exe().context("getting current exe path")?;
+    let task_xml_contents = format!(
+        include_str!("task.xml"),
+        exe_path = exe_path.to_str().context("os path was not valid unicode")?,
+    );
+    let task_xml_file = NamedTempFile::new().context("could not make temp file")?;
+    std::fs::write(&task_xml_file, task_xml_contents).context("could not write to temp file")?;
+    let task_xml_file_path = task_xml_file.into_temp_path();
+    let task_xml_file_path_str = task_xml_file_path
+        .to_str()
+        .context("os path was not valid unicode")?;
+    println!("path: {}", task_xml_file_path_str);
     let task_deleted = delete_existing_task()?;
     if task_deleted {
         println!("Deleted existing task to reinstall new one")
     }
     let output = Command::new("schtasks")
-        .args([
-            "/create",
-            "/sc",
-            "onevent",
-            "/mo",
-            "*[System[(EventID=42)]]",
-            "/EC",
-            "System",
-            "/tn",
-            TASK_NAME,
-            "/tr",
-            exe_path.to_str().context("path was not valid unicode")?,
-        ])
+        .args(["/create", "/tn", TASK_NAME, "/xml", task_xml_file_path_str])
         .output()
         .context("launching task scheduler")?;
     if output.status.success() {
         Ok(())
     } else {
         Err(anyhow!(
-            "failed to add task because {}",
-            String::from_utf8(output.stdout)?
+            "failed to add task because: {}\n{}",
+            String::from_utf8(output.stdout)?,
+            String::from_utf8(output.stderr)?
         ))
     }
 }
